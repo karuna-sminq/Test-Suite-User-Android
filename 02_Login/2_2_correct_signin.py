17bfa1a594c95628379028dd3ef980dd2e11b137
karuna <karunal@sminq.com>
2017-05-18 12:42:05 +0530

    Added test script for Login/correct signin

--- 02_Login/2_2_correct_signin.py
+++ 02_Login/2_2_correct_signin.py
@@ -1,12 +1,13 @@
 import unittest
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
+from random import randint
 import os
 from time import sleep
 from appium import webdriver
 import time
 
-class LaunchApp(unittest.TestCase):
+class Login(unittest.TestCase):
     def setUp(self):
         desired_caps = {}
         desired_caps['appium-version'] = '1.0'
@@ -14,22 +15,32 @@ class LaunchApp(unittest.TestCase):
         desired_caps['platformVersion'] = '6.0'
         desired_caps['deviceName'] = 'emulator-5554'
         desired_caps['app'] = os.path.abspath('/Users/sminq/Downloads/UserApp-v2.2.2-queuingSystem.apk')
-        desired_caps["session-override"] = 'true'
+        desired_caps["session-override"] = 'false'
 
         self.driver = webdriver.Remote('http://0.0.0.0:4723/wd/hub', desired_caps)
         self.driver.implicitly_wait(60)
 
-    def test_launch(self):
+    def test_correct_signin(self):
         self.driver.find_element_by_id("com.sminq.userbug:id/tv_next").click()
         self.driver.find_element_by_id("com.sminq.userbug:id/tv_next").click()
         self.driver.find_element_by_id("com.sminq.userbug:id/tv_next").click()
         self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
         self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
         self.driver.find_element_by_id("com.android.packageinstaller:id/permission_allow_button").click()
+
+        self.driver.find_element_by_id("com.sminq.userbug:id/edit_text_mobile_number").send_keys('7000000003')
+        self.driver.find_element_by_id("com.sminq.userbug:id/button_login").click()
+        self.driver.find_element_by_id("com.sminq.userbug:id/btn_enter_otp_manually").click()
+        self.driver.find_element_by_id("com.sminq.userbug:id/edit_text_otp").send_keys(989898)
+        self.driver.find_element_by_id("com.sminq.userbug:id/button_verify_account").click()
+
+        self.driver.find_element_by_id("android:id/button1").click()
+
+        print self.driver.find_element_by_id("com.sminq.userbug:id/tv_selected_city").get_attribute('text')
 
     def tearDown(self):
     	self.driver.quit()
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(LaunchApp)
+    suite = unittest.TestLoader().loadTestsFromTestCase(Login)
     unittest.TextTestRunner(verbosity=2).run(suite)
--- sminq_tests.sh
+++ sminq_tests.sh
@@ -3,7 +3,7 @@
 echo '''
 Copyright (C) 2016 Sminq India Solutions Pvt Ltd.
 Created on 2016-11-23
-Updated on 2017-05-17
+Updated on 2017-05-18
 
  ____    __  __   ___   _   _    ___
 / ___|  |  \/  | |_ _| | \ | |  / _ \
@@ -17,7 +17,7 @@ ___) |  | |  | |  | |  | |\  | | |_| |
 
 echo "================================="
 echo "Sminq User App v2.2.2 ..."
-echo "Running Test Suite v1.1 ..."
+echo "Running Test Suite v1.2 ..."
 echo "================================="
 
 d="$(date +'%d-%m-%Y')"
@@ -42,7 +42,7 @@ do
 
     #Execute file
     python $fname
-    # total_count=$((total_count + 1))
+    total_count=$((total_count + 1))
 
   done
 done
@@ -52,5 +52,5 @@ end=`date +%s`
 total_time=$((end-start))
 
 echo "================================="
-echo "Time taken for sminq test suite: $total_time sec."
+echo "Time taken for $total_count tests: $total_time sec."
 echo "================================="
